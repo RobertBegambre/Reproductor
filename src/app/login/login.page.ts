7c820c9a28a4d9f218c81755c77780d6c627274c
RobertBegambre <robertjmeza12@gmail.com>
2024-07-28 21:00:58 -0500

    email

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm=this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.email
+        ])
+      )
+    })
+   }
 
   ngOnInit() {
+  }
+
+  loginUser(dataLogin: any){
+    console.log(dataLogin)
   }
 
 }
